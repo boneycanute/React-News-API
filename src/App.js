8a46456d7967d74785f7688030063a2de3d994b0
Boney <boney.fernandez@collegedekho.com>
2023-06-01 12:57:56 +0530

    new api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { LinearProgress } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    var url =
+      "https://newsapi.org/v2/everything?q=technology&from=2023-05-01&sortBy=publishedAt&apiKey=ad2f8f6ee02b4b5daa12032ed92fb380";
+    var req = new Request(url);
+
+    fetch(req).then((response) => {
+      response.json().then((data) => {
+        setData(data);
+        setLoaded(true);
+        console.log(data);
+      });
+    });
+  }, []);
+  if (loaded) {
+    return (
+      <div className="ArticlesContainer">
+        {data.articles.map((article, index) => {
+          return (
+            <div className="article" key={index}>
+              <img className="thumbnail" src={article.urlToImage} />
+              <div className="text-content">
+                <p className="title">{article.title}</p>
+                <p className="content">{article.content}</p>
+                <p className="published">{article.publishedAt}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return <LinearProgress />;
+  }
 }
 
 export default App;
